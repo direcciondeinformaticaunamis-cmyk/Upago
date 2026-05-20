b4e4e1367894561bc3f2db2656e03b060d914f28
mjelectronicasr-dev <mjelectronicasr-dev@users.noreply.github.com>
2026-05-20 11:11:35 -0300

    feat: agregar script de prueba de conexion de base de datos

--- build_dist.bat
+++ build_dist.bat
@@ -20,6 +20,7 @@ copy /Y api.php dist\api.php
 copy /Y api-banco.php dist\api-banco.php
 copy /Y security.php dist\security.php
 copy /Y diagnostico.php dist\diagnostico.php
+copy /Y test-conexion.php dist\test-conexion.php
 copy /Y verificar_build.php dist\verificar_build.php
 copy /Y .htaccess dist\.htaccess
 copy /Y *.sql dist\
